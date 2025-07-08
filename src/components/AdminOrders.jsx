8d0b6cd66d8a9548981f454ba1d8ce1be92c6392
IgorSh2010 <igor.sh2010@gmail.com>
2025-07-08 22:14:10 +0200

    Оновлено з Sanity

--- src/components/AdminOrders.jsx
+++ src/components/AdminOrders.jsx
@@ -85,7 +85,7 @@ const AdminOrders = () => {
                 <p><strong className="text-pink-700">🆔 ID:</strong> {order.id}</p>
                 <p><strong className="text-pink-700">👤 Klient:</strong> {order.name} ({order.email})</p>
                 <p><strong className="text-pink-700">📞 Nr telefonu:</strong> {order.userPhone}</p>
-                <p><strong className="text-pink-700">📝 Uwagi:</strong> {order.notes}</p>
+                <p><strong className="text-pink-700 max-w-3xl">📝 Uwagi:</strong> {order.notes}</p>
 
                 <button
                   onClick={() => setExpandedId(expandedId === order.id ? null : order.id)}
